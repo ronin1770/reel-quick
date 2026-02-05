4dc3847a035750b7dddbb31d5d00e54955d3bad1
Farhan Munir <munir.farhan@gmail.com>
2026-02-05 11:26:47 +0500

    Updated code for ai engine integration

---
+++ backend/objects/ai_engine.py
@@ -0,0 +1,5 @@
+# This will serve as an engine for running AI queries
+# It will use langchain to orchareastrate calls
+# OpenAI key is provided in the .env file in a field called: OPENAI_API_KEY
+# Output type will be provided in the input (CSV / JSON) - use this to validate the output
+# Prompts are defined in backend / prompts / and its name will be provided in the input method
